1288957fb3cf02dcd737183fe4e7d5b15cd76984
Marcelo <marcelodossantosmachado05@gmail.com>
2025-10-17 00:33:16 -0300

    Feat:Fetch funcional de cards dos produtos cadastrado, com imagem.

--- JS/index.js
+++ JS/index.js
@@ -1,91 +1,57 @@
-//const produtosComId = produtos.map((p, index) => ({ ...p, id: index }));
+const mainProdutos = document.querySelector("#mainProdutos");
+const urlApi = "http://localhost:8085/produto/listar/todos";
+const urlImg = "http://localhost:8085/produto";
+const urlCategoria = "http://localhost:8085/produto";
 
-const senhaInput = document.getElementById("inputSenha");
-const filtros = document.getElementById("navCategorias");
-const inputBusca = document.getElementById("busca");
-const botaoLogin = document.getElementById("botaoLogin");
+//GET produtos home
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(urlApi)
+    .then((response) => response.json())
+    .then((data) => exibirProdutos(data));
+  function exibirProdutos(produtos) {
+    produtos.forEach((produto) => {
+      //Define quais elementos devem ser criados
+      const div = document.createElement("div");
+      const divCard = document.createElement("div");
+      const cardBody = document.createElement("div");
+      const img = document.createElement("img");
+      const title = document.createElement("h5");
+      const categoria = document.createElement("span");
+      const valor = document.createElement("p");
+      const descricao = document.createElement("p");
+      const btnCarrinho = document.createElement("a");
 
-//Função para filtrar os produtos
-/*function filtrarCategoria(categoria) {
-  if (!categoria) return produtosComId;
-  return produtosComId.filter((p) => p.categoria === categoria);
-}
+      //Define as listas de classes dos elementos para os cards
+      div.classList = "px-5 pb-3";
+      divCard.classList = "card my-3 mx-auto rounded-4";
+      cardBody.classList = "card-body";
+      img.classList = "card-img-top";
+      title.classList = "fw-bold";
+      categoria.classList = "badge w-25";
+      valor.classList = "text-primary fw-bold my-1";
+      btnCarrinho.classList = "btn btn-outline-primary w-100 mx-auto cardBtn";
 
-filtros.addEventListener("click", function (e) {
-  if (e.target.tagName === "IMG") {
-    Array.from(this.children).forEach((btn) => btn.classList.remove("active"));
-    e.target.classList.add("active");
-    const categoria = e.target.getAttribute("data-categoria");
-    exibirProdutos(filtrarCategoria(categoria));
+      //Chama a imagem através do endpoint
+      img.src = `${urlImg}/${produto.cdProduto}/imagem`;
+      img.alt = produto.nmProduto;
+
+      //Define os dados a serem renderizados
+      title.innerText = `${produto.nmProduto}`;
+      categoria.innerText = `${produto.dsCategoria}`;
+      valor.innerText = `R$ ${produto.vlProduto}`;
+      descricao.innerText = `${produto.dsProduto}`;
+      btnCarrinho.innerText = `Adicionar ao carrinho`;
+
+      //Define a ordem dos elementos
+      divCard.appendChild(img);
+      divCard.appendChild(cardBody);
+      cardBody.appendChild(title);
+      cardBody.appendChild(categoria);
+      cardBody.appendChild(valor);
+      cardBody.appendChild(descricao);
+      divCard.appendChild(div);
+      div.appendChild(btnCarrinho);
+      mainProdutos.appendChild(divCard);
+    });
   }
 });
-
-inputBusca.addEventListener("input", function () {
-  const filtroInput = this.value.toLowerCase();
-  const listaFiltrada = produtosComId.filter(
-    (produto) =>
-      produto.nome.toLowerCase().includes(filtroInput) ||
-      produto.categoria.toLowerCase().includes(filtroInput)
-  );
-  exibirProdutos(listaFiltrada);
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  exibirProdutos(produtosComId);
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("animate__animated", "animate__fadeInUp");
-        observer.unobserve(entry.target);
-      }
-    });
-  });
-
-  // Escolha o que quer animar:
-  document.querySelectorAll("#mainProdutos, .categoriasLi").forEach((el) => {
-    observer.observe(el);
-  });
-});*/
-
-//TESTE DE CONEXÃO COM O BACK-END
-const urlApi = "http://localhost:8085/produto/listar/todos";
-const mainProdutos = document.getElementById("mainProdutos");
-
-//GET
-fetch(urlApi)
-  .then((response) => {
-    response.json();
-  })
-  .then(function (myJson) {
-    const data = myJson.data;
-    data.forEach((post) => {
-      div.innerHTML =
-        '<div class="card pb-3 px-3 ">' +
-        '<img src="' +
-        post.imgProduto +
-        '" class="card-img-top" alt=""/>' +
-        '<div class="card-body">' +
-        '<span class="card-title fw-bold badge rounded-3 px-2" id="categoriaBagde">' +
-        post.dsCategoria +
-        "</span>" +
-        '<p class="card-title fw-bold mt-2">' +
-        post.nmProduto +
-        "</p>" +
-        '<p class="card-text fw-bold text-primary">' +
-        post.vlProduto +
-        "</p>" +
-        '<p class="card-text mb-3">' +
-        post.dsProduto +
-        "</p>" +
-        "</div>" +
-        '<a href="Pages/login.html" class="btn w-100 mx-auto mt-auto cardBtn">Adicionar ao carrinho</a>';
-      ("</div>");
-      mainProdutos.appendChild(div);
-    });
-  })
-  .catch((error) => {
-    mainProdutos.value = `Erro:  ${error.message}`;
-  });
