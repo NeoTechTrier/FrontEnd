0921111fb0189bb1ab313849ff678c059799a7d2
Marcelo <marcelodossantosmachado05@gmail.com>
2025-10-23 22:41:33 -0300

    Fix: Passando para o mesmo arquivo js

--- JS/index.js
+++ JS/index.js
@@ -272,3 +272,5 @@ const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
 const dropdownList = [...dropdownElementList].map(
   (dropdownToggleEl) => new bootstrap.Dropdown(dropdownToggleEl)
 );
+/*------------------------------------------------------------------------------------------------------*/
+
